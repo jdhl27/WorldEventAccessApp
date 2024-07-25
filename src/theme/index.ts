40a723819033ff30eda17f29b89082b2f9f2f3ca
jdhl27 <independientemedellin.27.03@gmail.com>
2024-07-25 02:38:13 -0500

    add your own theme for the design

---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+export const gradientDark = [
+  'rgba(44,60,124,1)',
+  'rgba(42,52,94,1)',
+  'rgba(24,29,49,1)',
+];
+
+export const gradientLight = [
+  'rgba(255,255,255,1)',
+  'rgba(239,241,255,1)',
+  'rgba(194,205,255,1)  ',
+];
---
+++ src/theme/light.ts
@@ -0,0 +1,24 @@
+export default {
+  background: '#fff',
+  secondaryBackground: '#f1f2ff',
+  reverseColor: '#181D31',
+  text: '#333333',
+  secondaryText: '#fff',
+  grayText: '#aaa',
+  primary: '#4461F2',
+  accent: '#ff5b60',
+  border: '#bdc3c7',
+  success: '#2ecc71',
+  error: '#e74c3c',
+  warning: '#f39c12',
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 4,
+  },
+};
